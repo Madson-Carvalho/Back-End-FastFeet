a70fba55d2249f14642d8f77073b3da68c2f7999
Madson-Carvalho <madsonraynnon@gmail.com>
2024-06-08 16:25:36 -0300

    Criados o controller de usuarios e rotas para users.

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,94 @@
+const prisma = require('../utils')
+const e = require("express");
+
+class UsersController {
+    async create(request, response) {
+        try {
+            const {name, email, cpf, password, perfil, phone} = request.body;
+
+            const user = await prisma.users.create({
+                data: {
+                    name: name,
+                    email: email,
+                    cpf: cpf,
+                    password: password,
+                    perfil: perfil,
+                    phone: phone
+                }
+            })
+
+            return response.json(user);
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+
+    async update(request, response) {
+        try {
+            const {name, email, cpf, password, perfil, phone} = request.body;
+            const {id} = request.params;
+
+            const editedUser = await prisma.users.update({
+                where: {
+                    id: id
+                },
+                data: {
+                    name: name,
+                    email: email,
+                    cpf: cpf,
+                    password: password,
+                    perfil: perfil,
+                    phone: phone
+                }
+            })
+
+            return response.json(editedUser);
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+
+    async delete(request, response) {
+        try {
+            const {id} = request.params;
+
+            const deletedUser = await prisma.users.delete({
+                where: {
+                    id: id
+                }
+            });
+
+            return response.json();
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+
+    async findAll(request, response) {
+        try {
+            const users = await prisma.users.findMany();
+
+            return response.json(users);
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+
+    async findById(request, response) {
+        try {
+            const {id} = request.params;
+
+            const user = await prisma.users.findUnique({
+                where: {
+                    id: id
+                }
+            });
+
+            return response.json(user);
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+}
+
+module.exports = UsersController;
---
+++ src/routes/users.routes.js
@@ -0,0 +1,15 @@
+const Router = require("express");
+
+const UsersController = require("../controllers/UsersController")
+
+const usersRoutes = Router()
+
+const usersController = new UsersController()
+
+usersRoutes.post('/create', usersController.create);
+usersRoutes.put('/edit/:id', usersController.update);
+usersRoutes.delete('/remove/:id', usersController.delete);
+usersRoutes.get('/find-all', usersController.findAll);
+usersRoutes.get('/find-by-id/:id', usersController.findById);
+
+module.exports = usersRoutes
