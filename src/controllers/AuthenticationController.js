8e2a4126670a0bccb8b9d6efb43afcb7bbc5fcde
GABRIEL MONTEIRO DE SOUZA <gabriel_m_de-souza@estudante.sc.senai.br>
2024-06-18 21:32:51 -0300

    implementado autenticação jwt nas rotas

---
+++ src/controllers/AuthenticationController.js
@@ -0,0 +1,32 @@
+const prisma = require('../utils')
+const jwt = require("jsonwebtoken");
+
+class AuthenticationController {
+    async login(request, response) {
+        try {
+            const {email, password} = request.body;
+
+            const user = await prisma.users.findUnique({
+                where:{
+                    password: password,
+                    email: email,
+                }
+            })
+
+            if (!user) {
+                return response.status(401).json({ error: 'User or Password Incorrected' });
+            }
+
+            const token = jwt.sign({ id: user.id }, env("SECRET_KEY"), {
+                expiresIn: '60m',
+            });
+
+            return response.status(200).json({ token });
+
+        } catch (e) {
+            return response.status(409).send();
+        }
+    }
+}
+
+module.exports = AuthenticationController;
