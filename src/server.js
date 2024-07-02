54a8ba0ad16bc90d9163f8d67b8d8d535b69580c
Madson-Carvalho <madsonraynnon@gmail.com>
2024-07-01 21:50:08 -0300

    Ajustando server pro deploy

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require('./routes');
 const cors = require("cors");
+require('dotenv/config');
 
 const app = express();
 const allowedOrigins = ['http://localhost:3000'];
@@ -15,5 +16,5 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
